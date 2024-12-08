5260e91c6238801fdcf5887bd0d417befbf44090
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-07 23:05:02 -0500

    doc: added documentation for to ReturnEvenNums class

--- ReturnEvenNums.java
+++ ReturnEvenNums.java
@@ -2,6 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReturnEvenNums {
+    /**
+     * Filters and returns a list of even numbers from the provided list.
+     *
+     * @param numbers The list of integers to filter.
+     * @return A list containing only the even numbers from the input list.
+     */
+
      public static List<Integer> getEvenNums(List<Integer> numbers) {
         List<Integer> evenNumbers = new ArrayList<>();
         for (Integer number : numbers) {
