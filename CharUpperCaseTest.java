b58239a27096c5e9c1cd9214ee3de9eccc5fbfed
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-08 19:09:44 -0500

    test: updated test case for converting a string yo uppercase

--- CharUpperCaseTest.java
+++ CharUpperCaseTest.java
@@ -1,13 +1,13 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Test;
-import org.junit.platform.commons.util.StringUtils;
+
 
 public class CharUpperCaseTest {
     @Test
      public void testToUpperCase() {
         String input = "ce noir";
         String expected = "CE NOIR";
-        String result = StringUtils.toUpperCase(input);
+        String result = CharUpperCase.toUpperCase(input);
         assertEquals("The string should be converted to uppercase", expected, result);
     }
 }
