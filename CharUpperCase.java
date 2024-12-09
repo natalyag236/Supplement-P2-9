161c4eae130b5f2ee390614daab81362690647fe
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-08 19:15:09 -0500

    doc: added documentation to CharUpperCase class

--- CharUpperCase.java
+++ CharUpperCase.java
@@ -1,5 +1,11 @@
 public class CharUpperCase {
-
+    /**
+     * Converts all characters in the given string to uppercase.
+     *
+     * @param input The string to be converted to uppercase.
+     * @return A new string with all characters converted to uppercase.
+     * @throws IllegalArgumentException if the input string is null.
+     */
     public static String toUpperCase(String input){
         if (input == null){
             throw new IllegalArgumentException("Input string cannot be null");
